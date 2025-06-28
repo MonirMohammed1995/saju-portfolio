85fe148418f9ff854526240954ac7f079472fc85
Monir Mohammed Nayem <monirmdnayemsaju42@gmail.com>
2025-06-29 00:01:54 +0600

    path modify

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -86,7 +86,7 @@ const Banner = () => {
           </div>
 
           <a
-                href="../../public/resume.pdf"
+                href="../../public/resume/resume.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="block w-full text-center px-4 py-2 rounded-md bg-gradient-to-r from-primary to-accent text-white font-semibold hover:scale-105 transition"
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -116,7 +116,7 @@ const Navbar = () => {
             ))}
             <div className="pt-2">
               <a
-                href="../../public/resume.pdf"
+                href="../../public/resume/resume.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="block w-full text-center px-4 py-2 rounded-md bg-gradient-to-r from-primary to-accent text-white font-semibold hover:scale-105 transition"
